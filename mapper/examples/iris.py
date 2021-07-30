bec717e1c085e4378a8fa6a6251d213a8dd75f9d
Robotmurlock <momir.adzemovic@gmail.com>
2021-07-30 13:29:10 +0200

    Update Iris example plot output

--- mapper/examples/iris.py
+++ mapper/examples/iris.py
@@ -34,19 +34,28 @@ def run():
     for name, data in graph.nodes.data():
         instances = data['instances']
         instance_classes = y[instances]
-        modes.append(instance_classes.mode()[0])
-        entropies.append(entropy(instance_classes.value_counts() / instance_classes.shape[0]))
+        modes.append([name, instance_classes.mode()[0]])
+        entropies.append([name, entropy(instance_classes.value_counts() / instance_classes.shape[0])])
 
     mode_colors = {
         'Iris-setosa': 'red',
         'Iris-virginica': 'blue',
         'Iris-versicolor': 'green'
     }
-    mode_color_map = [mode_colors[m] for m in modes]
 
     pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, node_color=mode_color_map, with_labels=True)
-    plt.show()
+    fig, axs = plt.subplots(figsize=(14, 8), ncols=2)
+
+    axs[0].set_title('Graph colored by class mode')
+    for c in ['Iris-setosa', 'Iris-virginica', 'Iris-versicolor']:
+        nx.draw_networkx_nodes(graph, pos,
+            ax=axs[0],
+            node_color=mode_colors[c],
+            nodelist=[m[0] for m in modes if m[1] == c],
+            label=c)
+    nx.draw_networkx_edges(graph, pos=pos, ax=axs[0])
+    nx.draw_networkx_labels(graph, pos, ax=axs[0])
+    axs[0].legend(scatterpoints=1)
 
     entropy_colors = {
         0.0: 'beige',
@@ -56,16 +65,21 @@ def run():
         0.4: 'coral',
         1.0: 'red'
     }
-    entropy_color_map = []
-    for e in entropies:
-        for treshold, color in entropy_colors.items():
-            if e <= treshold:
-                entropy_color_map.append(color)
-                break
+    axs[1].set_title('Graph colored by node entropy')
+    used_nodes = []
+    for treshold, color in entropy_colors.items():
+        nodelist = [e[0] for e in entropies if e[1] <= treshold and e[0] not in used_nodes]
+        used_nodes = used_nodes + nodelist
+        nx.draw_networkx_nodes(graph, pos,
+            ax=axs[1],
+            node_color=color,
+            nodelist=nodelist,
+            label=f'<= {treshold}')
+    nx.draw_networkx_edges(graph, pos=pos, ax=axs[1])
+    nx.draw_networkx_labels(graph, pos, ax=axs[1])
+    axs[1].legend(scatterpoints=1)
 
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, node_color=entropy_color_map, with_labels=True)
-    plt.show()
+    fig.savefig('../../docs/images/iris.png')
 
 
 if __name__ == '__main__':
