6b4291eb77c65119e05e3a968d2470d7e809c5ec
Robotmurlock <momir.adzemovic@gmail.com>
2021-08-16 18:54:57 +0200

    Test Mapper algorithm with cosine similarity and Td-Idf

--- 02_tweet_disaster/main.py
+++ 02_tweet_disaster/main.py
@@ -4,8 +4,11 @@ from vocabulary import Vocabulary
 import numpy as np
 import kmapper as km
 from sklearn.manifold import TSNE
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def kmapper_create_visualization(
@@ -50,6 +53,48 @@ def kmapper_averaged_token_vectors(df: pd.DataFrame, vocabulary: Vocabulary) -> 
     kmapper_create_visualization(mapper, graph, df, 'averaged_token_vectors')
 
 
+def kmapper_tfidf_pca_projection(df: pd.DataFrame) -> None:
+    X = TfidfVectorizer().fit_transform(df.TEXT).toarray()
+    mapper = km.KeplerMapper(verbose=1)
+    lens = mapper.fit_transform(X, projection=PCA(n_components=3, random_state=42))
+    graph = mapper.map(lens, X,
+                       clusterer=DBSCAN(eps=2.0),
+                       cover=km.Cover(n_cubes=8, perc_overlap=0.5))
+
+    kmapper_create_visualization(mapper, graph, df, 'tfidf_pca')
+
+
+def kmapper_tfidf_pca_projection_with_cosine_distance_and_dbscan_clustering(df: pd.DataFrame) -> None:
+    X = TfidfVectorizer().fit_transform(df.TEXT).toarray()
+
+    interpoint_distance_matrix = 1 - cosine_similarity(X)
+
+    mapper = km.KeplerMapper(verbose=1)
+    lens = mapper.fit_transform(X, projection=PCA(n_components=2, random_state=42),
+                                distance_matrix=interpoint_distance_matrix)
+    graph = mapper.map(lens, X,
+                       clusterer=DBSCAN(eps=1.0),
+                       cover=km.Cover(n_cubes=8, perc_overlap=0.5))
+
+    kmapper_create_visualization(mapper, graph, df, 'tfidf_pca_cd_dbscan')
+
+
+def kmapper_tfidf_pca_projection_with_cosine_distance_and_hierarchical_clustering(df: pd.DataFrame) -> None:
+    X = TfidfVectorizer().fit_transform(df.TEXT).toarray()
+
+    interpoint_distance_matrix = 1 - cosine_similarity(X)
+
+    for linkage in ['single', 'average', 'complete', 'ward']:
+        mapper = km.KeplerMapper(verbose=1)
+        lens = mapper.fit_transform(X, projection=PCA(n_components=2, random_state=42),
+                                    distance_matrix=interpoint_distance_matrix)
+        graph = mapper.map(lens, X,
+                           clusterer=AgglomerativeClustering(n_clusters=2, linkage=linkage),
+                           cover=km.Cover(n_cubes=10, perc_overlap=0.5))
+
+        kmapper_create_visualization(mapper, graph, df, f'tfidf_pca_cd_{linkage}')
+
+
 def run():
     df = pd.read_csv('../data/tweet_disaster.csv')
     df = transform(df)
@@ -63,6 +108,9 @@ def run():
 
     kmapper_averaged_token_vectors(df, vocabulary)
     kmapper_maximum_of_token_vectors(df, vocabulary)
+    kmapper_tfidf_pca_projection(df)
+    kmapper_tfidf_pca_projection_with_cosine_distance_and_dbscan_clustering(df)
+    kmapper_tfidf_pca_projection_with_cosine_distance_and_hierarchical_clustering(df)
 
 
 if __name__ == '__main__':
